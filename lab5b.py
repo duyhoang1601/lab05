ae1a833a974d5ef473633018998b514209a5a493
duyhoang1601 <155625243+duyhoang1601@users.noreply.github.com>
2024-03-01 21:04:25 +0700

    Add files via upload

---
+++ lab5b.py
@@ -0,0 +1,103 @@
+#Q1 lab5b
+class Vehicle:
+    def __init__(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+modelX = Vehicle(240, 18)
+print(modelX.max_speed, modelX.mileage)
+class Vehicle:
+    pass
+
+#Q2.1 lab5b
+class Vehicle:
+
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+#Q2.2 lab5b
+class Vehicle:
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+    def seating_capacity(self, capacity):
+        return f"The seating capacity of a {self.name} is {capacity} passengers"
+
+class Bus(Vehicle):
+    def seating_capacity(self, capacity=50):
+        return super().seating_capacity(capacity=50)
+
+School_bus = Bus("School Volvo", 180, 12)
+print(School_bus.seating_capacity())
+
+#Q3.1
+class Vehicle:
+    # Class attribute
+    color = "White"
+
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+class Bus(Vehicle):
+    pass
+
+class Car(Vehicle):
+    pass
+
+School_bus = Bus("School Volvo", 180, 12)
+print(School_bus.color, School_bus.name, "Speed:", School_bus.max_speed, "Mileage:", School_bus.mileage)
+
+car = Car("Audi Q5", 240, 18)
+print(car.color, car.name, "Speed:", car.max_speed, "Mileage:", car.mileage)
+
+#Q3.2
+class Vehicle:
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+    def seating_capacity(self, capacity):
+        return f"The seating capacity of a {self.name} is {capacity} passengers"
+
+class Bus(Vehicle):
+    # assign default value to capacity
+    def seating_capacity(self, capacity=50):
+        return super().seating_capacity(capacity=50)
+
+School_bus = Bus("School Volvo", 180, 12)
+print(School_bus.seating_capacity())
+
+#Q4.1
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+class Bus(Vehicle):
+    pass
+
+School_bus = Bus("School Volvo", 12, 50)
+
+print(type(School_bus))
+
+#Q4.2
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+class Bus(Vehicle):
+    pass
+
+School_bus = Bus("School Volvo", 12, 50)
+
+print(isinstance(School_bus, Vehicle))
